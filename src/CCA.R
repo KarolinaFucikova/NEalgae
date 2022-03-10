d8c43c78411d62f2fcd8a251e6eec511d994c47a
Karolina Fucikova <karolina.fucikova@gmail.com>
2022-03-10 16:42:58 -0500

    graphing updates

--- src/CCA.R
+++ src/CCA.R
@@ -1,3 +1,9 @@
 # test of canonical correspondence analysis using vegan
 # to be applied to community/chemistry data sets
 # graphing options with ggvegan
+# installing ggvegan is a little tricky it seems:
+# install.packages("remotes")
+# remotes::install_github("gavinsimpson/ggvegan")
+library("vegan")
+library("ggvegan")
+
--- src/historic_desmids.R
+++ src/historic_desmids.R
@@ -43,6 +43,8 @@ plot(occ_hc)
 # trying detrended correspondence analysis
 # probably don't have enough data for it but seems most appropriate according to
 # https://www.davidzeleny.net/anadat-r/doku.php/en:ca_dca
+# there are a lot of zeroes, so PCA and similar analyses are not ideal
+# it's only presence/absence data, not abundance or relative abundance
 # again have to exclude HB1967 as the three species that are found there will look like outliers
 
 ord <- decorana(t(clean_occ))
@@ -54,12 +56,28 @@ plot(ord, type = "n")
 points(ord, display = "sites", cex = 0.8, pch=21, col="red", bg="yellow")
 text(ord, display = "spec", cex=0.7, col="blue")
 
+# attempting to label just particular points, automatically without the locator function
+# can get species coordinates from the summary table
+# https://www.rdocumentation.org/packages/vegan/versions/2.4-2/topics/scores
+# here the species farthest along axes are:
+species_toplot <- c("Desmidium_asymmetricum")
+# this list can be changed, added to, etc.)
+# it won't automatically plot but makes it easier to find coordinates
+
+table_toplot <- scores(ord, display="species")
+# just the first two DCAs"
+table_toplot <- table_toplot[,c(1:2)]
+# make a table of just the DCA1 and 2 coordinates of just the species on the list
+species_toplot_table <- table_toplot[rownames(table_toplot) %in% species_toplot, ]
+
+
 # apparently can add the clustergram
 plot(ord, type = "n", ylim = c(-2,3), xlim = c(-2,3))
 ordicluster(ord,occ_hc)
 points(ord, display = "sites", pch = 2, col="#56B4E9", cex = 1.5)
 points(ord, display = "species", pch = 1, col="#0072B2", cex = 1)
 text(ord, display = "sites", cex=1, col="black", adj = c(1.2,0))
+#points(-2.2656238, -2.121114, col = "magenta", pch=19)
 legend(3, 2, c("sites", "species"),
        col=c("#56B4E9", "#0072B2"), pch=c(2,1), cex=1)
 # not adding species because that makes the plot illegible
@@ -117,34 +135,34 @@ fortify_plot
 # trying to plot with labels for notable genera; too many points overlap to be visible
 fort_species$genus <- desmids$Genus
 # comment out the genus that you WANT to be labeled
-fort_species$genus[fort_species$genus == "Actinotaenium"] <- "" 
-fort_species$genus[fort_species$genus == "Bambusina"] <- "" 
-fort_species$genus[fort_species$genus == "Closterium"] <- "" 
-fort_species$genus[fort_species$genus == "Cosmarium"] <- "" 
-fort_species$genus[fort_species$genus == "Cylindrocystis"] <- "" 
-#fort_species$genus[fort_species$genus == "Desmidium"] <- ""
-fort_species$genus[fort_species$genus == "Docidium"] <- "" 
-fort_species$genus[fort_species$genus == "Euastrum"] <- "" 
-fort_species$genus[fort_species$genus == "Gonatozygon"] <- "" 
-fort_species$genus[fort_species$genus == "Haplotaenium"] <- "" 
-fort_species$genus[fort_species$genus == "Hyalotheca"] <- "" 
-fort_species$genus[fort_species$genus == "Mesotaenium"] <- "" 
-fort_species$genus[fort_species$genus == "Micrasterias"] <- ""
-fort_species$genus[fort_species$genus == "Netrium"] <- ""
-fort_species$genus[fort_species$genus == "Onychonema"] <- "" 
-fort_species$genus[fort_species$genus == "Penium"] <- "" 
-fort_species$genus[fort_species$genus == "Pleurotaenium"] <- "" 
-fort_species$genus[fort_species$genus == "Roya"] <- ""
-fort_species$genus[fort_species$genus == "Spinoclosterium"] <- "" 
-fort_species$genus[fort_species$genus == "Spinocosmarium"] <- "" 
-fort_species$genus[fort_species$genus == "Spirotaenia"] <- "" 
-fort_species$genus[fort_species$genus == "Spondylosium"] <- "" 
-fort_species$genus[fort_species$genus == "Staurastrum"] <- "" 
-fort_species$genus[fort_species$genus == "Staurodesmus"] <- "" 
-fort_species$genus[fort_species$genus == "Teilingia"] <- "" 
-fort_species$genus[fort_species$genus == "Tetmemorus"] <- "" 
-fort_species$genus[fort_species$genus == "Triploceras"] <- ""
-fort_species$genus[fort_species$genus == "Xanthidium"] <- "" 
+fort_species$genus[fort_species$genus == "Actinotaenium"] <- "other" 
+fort_species$genus[fort_species$genus == "Bambusina"] <- "other" 
+fort_species$genus[fort_species$genus == "Closterium"] <- "other" 
+fort_species$genus[fort_species$genus == "Cosmarium"] <- "other" 
+fort_species$genus[fort_species$genus == "Cylindrocystis"] <- "other" 
+fort_species$genus[fort_species$genus == "Desmidium"] <- "other"
+#fort_species$genus[fort_species$genus == "Docidium"] <- "other" 
+fort_species$genus[fort_species$genus == "Euastrum"] <- "other" 
+fort_species$genus[fort_species$genus == "Gonatozygon"] <- "other" 
+fort_species$genus[fort_species$genus == "Haplotaenium"] <- "other" 
+#fort_species$genus[fort_species$genus == "Hyalotheca"] <- "other" 
+fort_species$genus[fort_species$genus == "Mesotaenium"] <- "other" 
+fort_species$genus[fort_species$genus == "Micrasterias"] <- "other"
+fort_species$genus[fort_species$genus == "Netrium"] <- "other"
+#fort_species$genus[fort_species$genus == "Onychonema"] <- "other" 
+fort_species$genus[fort_species$genus == "Penium"] <- "other" 
+fort_species$genus[fort_species$genus == "Pleurotaenium"] <- "other" 
+fort_species$genus[fort_species$genus == "Roya"] <- "other"
+fort_species$genus[fort_species$genus == "Spinoclosterium"] <- "other" 
+fort_species$genus[fort_species$genus == "Spinocosmarium"] <- "other" 
+fort_species$genus[fort_species$genus == "Spirotaenia"] <- "other" 
+fort_species$genus[fort_species$genus == "Spondylosium"] <- "other" 
+fort_species$genus[fort_species$genus == "Staurastrum"] <- "other" 
+fort_species$genus[fort_species$genus == "Staurodesmus"] <- "other" 
+#fort_species$genus[fort_species$genus == "Teilingia"] <- "other" 
+fort_species$genus[fort_species$genus == "Tetmemorus"] <- "other" 
+fort_species$genus[fort_species$genus == "Triploceras"] <- "other"
+fort_species$genus[fort_species$genus == "Xanthidium"] <- "other" 
 
 fortify_plot4 <- ggplot() +
   geom_point(data = fort_species, aes(x = DCA1, y = DCA2)) +
@@ -152,8 +170,10 @@ fortify_plot4 <- ggplot() +
   geom_vline(xintercept = c(0), color = "grey70", linetype = 2) +
   geom_hline(yintercept = c(0), color = "grey70", linetype = 2) +
   geom_text(data = fort_sites, aes(x = DCA1, y = DCA2, label = Label), nudge_x = 0.25, nudge_y = 0.25) +
-  geom_text(data = fort_species, aes(x = DCA1, y = DCA2, label = genus), nudge_x = 0.25, nudge_y = 0.25) +
+  #geom_text(data = fort_species, aes(x = DCA1, y = DCA2, label = genus), nudge_x = 0.25, nudge_y = 0.25) +
+  geom_point(data = fort_species, aes(x = DCA1, y = DCA2, color = genus)) +
   labs(x = "DCA1",
        y = "DCA2",
        title = "DCA - Historical and current desmid communities")
 fortify_plot4
+# too many points overlap, can't see the genera
